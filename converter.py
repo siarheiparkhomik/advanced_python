3a7d3e2f2d612ef184a4b8d039658fd5c5f636f6
Siarhei Parkhomik <siarhei_parkhomik@epam.com>
2019-01-28 23:39:14 +0300

    Homework 6

---
+++ converter.py
@@ -0,0 +1,23 @@
+"""Module contains converter realization
+"""
+import json
+import requests
+
+
+class Converter:
+    """Class that represents converter object
+    """
+    URL = "http://free.currencyconverterapi.com/"
+
+    @classmethod
+    def get_currencies_ratio(cls, from_currency, to_currency):
+        """Return the ration of two specified currencies
+        :param from_currency: the currency to convert from
+        :param to_currency: the currency to convert to
+        :return: currencies ratio
+        """
+        currencies = "{}_{}".format(from_currency, to_currency)
+        request = "{}api/v5/convert?q={}&compact=ultra".format(cls.URL,
+                                                               currencies)
+        responce = requests.get(request)
+        return json.loads(responce.text)[currencies]
---
+++ money.py
@@ -0,0 +1,48 @@
+"""Contains Money class realization
+"""
+from converter import Converter
+
+
+class Money:
+    """Class represents money object
+    """
+    CONVERTER = Converter()
+
+    def __init__(self, value, currency="EUR"):
+        self.value = value
+        self.currency = currency
+
+    def __mul__(self, other):
+        value = self.value * other
+        return __class__(value, self.currency)
+
+    def __rmul__(self, other):
+        value = self.value * other
+        return self.__class__(value, self.currency)
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            currency_multiplier = self.CONVERTER.get_currencies_ratio(
+                other.currency,
+                self.currency)
+            value = self.value + other.value * currency_multiplier
+            return self.__class__(value, self.currency)
+        else:
+            return self + self.__class__(other, self.currency)
+
+    def __radd__(self, other):
+        if isinstance(other, (int, float)):
+            return self.__class__(other, self.currency) + self
+        return self
+
+    def __str__(self):
+        return "{} {}".format(self.value, self.currency)
+
+
+if __name__ == "__main__":
+    x = Money(10, "BYN")
+    y = Money(11)
+    z = Money(12.34, "EUR")
+    print(z + 3.11 * x + y * 0.8)
+    lst = [Money(10, "BYN"), Money(11), Money(12.01, "JPY")]
+    print(sum(lst))
