4a55b4caef4ec1786457a9968f056aded2fa21e7
Siarhei Parkhomik <siarhei_parkhomik@epam.com>
2019-02-24 19:46:33 +0300

    homework 7-8

--- homework7_8.py
+++ homework7_8.py
@@ -1,23 +1,22 @@
 import re
 
 
-def generate_properties(arg_dict):
-    """
-    Check if dict includes specific methods and if yes, creates properties
+def generate_properties(args):
+    """Check if dict includes specific methods and if yes, creates properties
     according the names of this methods
 
-    :param arg_dict: dict oof class arguments
+    :param args: dict oof class arguments
     :return: dict with added properties
     """
     suitable_funcs = list(
-        filter(lambda x: x.startswith(("get", "set", "del")), arg_dict))
+        filter(lambda x: x.startswith(("get", "set", "del")), args))
     prop_names_set = {re.search(".*_(.+)", function_name).group(1) for
                       function_name in suitable_funcs}
     for prop_name in prop_names_set:
-        arg_dict[prop_name] = property(arg_dict.get("get_{}".format(prop_name)),
-                                       arg_dict.get("set_{}".format(prop_name)),
-                                       arg_dict.get("del_{}".format(prop_name)))
-    return arg_dict
+        args[prop_name] = property(args.get("get_{}".format(prop_name)),
+                                   args.get("set_{}".format(prop_name)),
+                                   args.get("del_{}".format(prop_name)))
+    return args
 
 
 class MetaClass(type):
